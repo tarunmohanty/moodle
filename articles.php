d90e8eba788a130fc7088b65b79f30b9f3fd817b
tarun mohanty <tarunmohantys@gmail.com>
2018-07-26 17:07:56 +0530

    adding articles php file

--- articles.php
+++ articles.php
@@ -6,7 +6,7 @@ $PAGE->set_context(get_system_context());
 $PAGE->set_pagelayout('standard');
 $PAGE->set_title("Your title");
 $PAGE->set_heading("Blank page");
-$PAGE->set_url($CFG->wwwroot.'/feedback.php');
+$PAGE->set_url($CFG->wwwroot.'/articles.php');
 
 echo $OUTPUT->header();
 echo "Hello World";
